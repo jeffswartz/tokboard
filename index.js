de44bccc9e49cd5327ff4b21cbcc0d249a954930
Dave Mun <dmun@tokbox.com>
2018-09-12 15:53:39 -0700

    add tls

--- index.js
+++ index.js
@@ -1,8 +1,38 @@
-var express = require('express');
-var app = express();
-var path = require('path');
+// Dependencies
+const fs = require('fs');
+const http = require('http');
+const https = require('https');
+const express = require('express');
 
-app.use(express.static(path.join(__dirname, 'public')));
+const app = express();
 
-app.listen(80);
-console.log('Listening on port 80');
+// Certificate
+const privateKey = fs.readFileSync('/etc/letsencrypt/live/tokboard.com/privkey.pem', 'utf8');
+const certificate = fs.readFileSync('/etc/letsencrypt/live/tokboard.com/cert.pem', 'utf8');
+const ca = fs.readFileSync('/etc/letsencrypt/live/tokboard.com/chain.pem', 'utf8');
+
+const credentials = {
+  key: privateKey,
+  cert: certificate,
+  ca: ca
+};
+
+app.use((req, res) => {
+  res.send('Hello there !');
+});
+
+// Starting both http & https servers
+const httpServer = http.createServer(app);
+http.get('*', function(req, res) {
+    res.redirect('https://' + req.headers.host + req.url);
+})
+
+const httpsServer = https.createServer(credentials, app);
+
+httpServer.listen(80, (req, res) => {
+  console.log('HTTP Server running on port 80, will redirect to HTTPS');
+});
+
+httpsServer.listen(443, () => {
+  console.log('HTTPS Server running on port 443');
+});
