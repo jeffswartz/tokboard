bac7d0d83b7f068cff14d4c54a7dd0fc237dc724
Dave Mun <dmun@tokbox.com>
2018-09-12 22:48:34 -0700

    cleanup

--- index.js
+++ index.js
@@ -4,35 +4,40 @@ const http = require('http');
 const path = require('path');
 const https = require('https');
 const express = require('express');
+const IS_PROD = process.env.IS_PROD || false;
 
-const app = express();
-const httpRedirect = express();
 
-// Certificate
-const privateKey = fs.readFileSync('/etc/letsencrypt/live/tokboard.com/privkey.pem', 'utf8');
-const certificate = fs.readFileSync('/etc/letsencrypt/live/tokboard.com/cert.pem', 'utf8');
-const ca = fs.readFileSync('/etc/letsencrypt/live/tokboard.com/chain.pem', 'utf8');
+if (IS_PROD) {
+  const httpsApp = express();
+  const privateKey = fs.readFileSync('/etc/letsencrypt/live/tokboard.com/privkey.pem', 'utf8');
+  const certificate = fs.readFileSync('/etc/letsencrypt/live/tokboard.com/cert.pem', 'utf8');
+  const ca = fs.readFileSync('/etc/letsencrypt/live/tokboard.com/chain.pem', 'utf8');
 
-const credentials = {
-  key: privateKey,
-  cert: certificate,
-  ca: ca
-};
+  const credentials = {
+    key: privateKey,
+    cert: certificate,
+    ca: ca
+  };
 
-app.use(express.static(path.join(__dirname, 'public')));
+  httpsApp.use(express.static(path.join(__dirname, 'public')));
+  const httpsServer = https.createServer(credentials, httpsApp);
+  httpsServer.listen(443, () => {
+    console.log('HTTPS Server running on port 443');
+  });
+}
 
-// Starting both http & https servers
-const httpServer = http.createServer(httpRedirect);
-httpRedirect.get("*", function (req, res, next) {
-    res.redirect("https://tokboard.com" + req.path);
+const httpApp = express();
+if (IS_PROD) {
+  console.log('HTTP Server running as prod config, will redirect to HTTPS');
+  httpApp.get('*', function (req, res, next) {
+      res.redirect("https://tokboard.com" + req.path);
+  });
+} else {
+  console.log('HTTP Server running as dev config, will serve static content');
+  httpApp.use(express.static(path.join(__dirname, 'public')));
+}
+const httpServer = http.createServer(httpApp);
+httpServer.listen(8080, () => {
+  console.log('HTTP Server running on port 80');
 });
 
-const httpsServer = https.createServer(credentials, app);
-
-httpServer.listen(80, () => {
-  console.log('HTTP Server running on port 80, will redirect to HTTPS');
-});
-
-httpsServer.listen(443, () => {
-  console.log('HTTPS Server running on port 443');
-});
